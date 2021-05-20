fc51466b195c4abf06dd7273c1a66734ac564ccb
Kaitlyn Midori <katyluhoo@yahoo.com>
2021-05-19 18:01:44 -0700

    troubleshooting js function

---
+++ .history/script_20210519145254.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$(document).ready(function() {
+    $('.hover').on('touchstart touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+    });
+});
