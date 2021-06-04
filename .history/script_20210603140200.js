4b8e05ba6c9e0955b03d03873e6b51c2f364df5f
Kaitlyn Midori <katyluhoo@yahoo.com>
2021-06-04 11:18:49 -0700

    project card link edit

---
+++ .history/script_20210603140200.js
@@ -0,0 +1,14 @@
+// const toggle = document.querySelector('.toggle')
+// const navigation = document.querySelector('.navigation')
+
+// toggle.addEventListener('click', () => {
+//   toggle.classList.toggle('active')
+//   navigation.classList.toggle('active')
+// })
+
+const btn = document.querySelector('.btn-primary')
+
+
+btn.addEventListener('click', () => {
+    btn.classList.btn('active')
+})
